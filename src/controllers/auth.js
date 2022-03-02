d152e98603588bea7c072cbdefeda3b7802dc61c
Idhibhat Pankam <marginpankam@gmail.com>
2022-03-02 14:30:27 +0700

    added create jwt function when register

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,7 +2,8 @@ const User = require("../models/User");
 
 const register = async (req, res) => {
 	const user = await User.create({ ...req.body });
-	res.status(200).json(req.body);
+	const token = user.createJWT();
+	res.status(200).json({ user: { name: user.name }, token });
 };
 const login = (req, res) => {
 	res.status(200).json(req.body);
